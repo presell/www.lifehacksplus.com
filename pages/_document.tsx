ca879a249b2c579728bcd59fd2e8d1c08a4c90f9
Devyn <devyn@presell.ai>
2023-03-03 19:08:49 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,14 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-export default class CustomDocument extends Document {
-    render(): JSX.Element {
+class CustomDocument extends Document {
+    render() {
         return (
         <Html>
             <Head> 
             </Head>
             
             <body>
-            <script src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RnfsUY"></script>
-
+            <Script src="https://static.klaviyo.com/onsite/js/klaviyo.js?company_id=RnfsUY" />
                 <Main />
                 <NextScript />
             </body>
