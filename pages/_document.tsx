358acd7136de0c4ed0212c0a85718190754e01df
Devyn <devyn@presell.ai>
2023-03-06 22:48:58 -0500

    update GTM measurement ID
    GTM-KR4VSHG

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,7 +15,7 @@ class MyDocument extends Document {
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-K7ZPTZR');
+              })(window,document,'script','dataLayer','GTM-KR4VSHG');
               `,
             }}
           />
@@ -23,7 +23,7 @@ class MyDocument extends Document {
         <body>
           <noscript>
             <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-K7ZPTZR"
+              src="https://www.googletagmanager.com/ns.html?id=GTM-KR4VSHG"
               height="0"
               width="0"
               style={{ display: 'none', visibility: 'hidden' }}
